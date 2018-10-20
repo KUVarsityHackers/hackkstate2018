e889274360203f13fa28c000f9d77169105c160d
czfloyd <floyd.c23@gmail.com>
2018-10-20 11:00:23 -0500

    Added basic Submissions page

--- photo-code/src/app/submissions/submissions.component.ts
+++ photo-code/src/app/submissions/submissions.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submissions.component.css']
 })
 export class SubmissionsComponent implements OnInit {
+  supportedLanguages: String[] = ['C++', 'Java'];
+  submissions: Submission[] = [];
+  selectedSubmission: Submission;
+  formData: any = {
+    fromdate: Date,
+    todate: Date,
+    language: String,
+    code: String
+  };
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  uploadSubmission() {
+    let s = new Submission()
+    this.selectedSubmission = s;
+    this.submissions.push(s);
+  }
+
+}
+
+class Submission {
+  image: String;
+  code: String;
+  output: String;
+  name: String;
+
+  constructor() {
+    this.image = "https://www.maxpixel.net/static/photo/1x/Photoshop-Coding-Code-Programming-Php-Web-Symbol-3647303.jpg";
+    this.code = "int main(){}";
+    this.output = "out";
+    this.name = "Daniel's Submission"
+  }
 }
