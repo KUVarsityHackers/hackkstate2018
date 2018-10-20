a261f985b2a8f63219b8a7c02ad19919ff91bc31
Nathan Nichols <n545n545@ku.edu>
2018-10-20 11:07:39 -0500

    added JDoodle API

--- photo-code/src/app/submissions/submissions.component.ts
+++ photo-code/src/app/submissions/submissions.component.ts
@@ -27,6 +27,27 @@ export class SubmissionsComponent implements OnInit {
     this.selectedSubmission = s;
     this.submissions.push(s);
   }
+	var recBody;
+	function CompileAndRun(arguments){
+		var request = require('request');
+
+		var program = {
+			script : arguments,
+			language: "cpp14",
+			versionIndex: "2",
+			clientId: "43540fa13cb920a980bb8011740ba63f",
+			clientSecret:"463414f3fccd12261c2afcff044d17937207c1f8c1c1b60a3ee181b314c6cfda"
+		};
+		request({
+			url: 'https://api.jdoodle.com/execute',
+			method: "POST",
+			json: program
+			},
+		function (error, response, body) {
+			recBody = body;
+			
+		});
+	}
 
 }
 
