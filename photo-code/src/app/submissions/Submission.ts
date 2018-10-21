f7c8dfa17029aaa90ad9c142843f2016a253a84e
czfloyd <floyd.c23@gmail.com>
2018-10-20 19:30:53 -0500

    Put most of the Submissions page together

---
+++ photo-code/src/app/submissions/Submission.ts
@@ -0,0 +1,13 @@
+export class Submission {
+    image;
+    code: String;
+    output: String;
+    name: String;
+  
+    constructor() {
+      this.image = "https://www.maxpixel.net/static/photo/1x/Photoshop-Coding-Code-Programming-Php-Web-Symbol-3647303.jpg";
+      this.code = "int main(){}";
+      this.output = "out";
+      this.name = "John Smith"
+    }
+  }
